a8dd573fddba1c2e155a2a8ef3f78d80a0784cf0
Reenku Choudhary <105987075+Reenku19@users.noreply.github.com>
2024-09-22 23:11:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx,vue}', // Adjust the paths and file extensions as needed
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
