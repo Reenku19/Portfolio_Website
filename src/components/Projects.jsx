205bfb82f47cfc648dceb9770e5ff7123e23e820
Reenku Choudhary <105987075+Reenku19@users.noreply.github.com>
2024-09-23 22:28:15 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,23 +41,24 @@ export const Projects = () => {
             image: p6,
             github: "https://github.com/Reenku19/Reenu_Boutique.git"
         },
-         {
+        {
             id: 7,
             image: p7,
             github: "https://github.com/Reenku19/Portfolio_Website.git"
         },
+        
     ]
     return (
         <div name="Projects" className='w-full md:min-h-screen bg-black' id="project">
             <div className='max-w-screen-lg p-4 h-full w-full mx-auto flex flex-col justify-center items-center md:items-start text-white'>
-                <div className='pb-8'>
-                    <h2 className='text-4xl sm:text-5xl font-bold inline border-b-4 border-gray-500' >Projects</h2>
+                <div className='mx-auto px-6 md:px-12 lg:px-18'>
+                    <h2 className='text-4xl font-bold text-center mb-12' >Projects</h2>
                 </div>
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
                     {projects.map(({ id, image, github }) => (
                         <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                             <img src={image}></img>
-                            <div className='flex items-center justify-evenly p-2'>
+                            <div className='flex items-center justify-evenly p-4'>
                                 <a href={github} className='hover:scale-110 duration-200'>
                                     {" "}
                                     <FaGithub size={30}></FaGithub>
