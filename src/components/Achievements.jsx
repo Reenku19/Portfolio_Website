0b487b122f949db900117d7c65c41226f92e5928
Reenku Choudhary <105987075+Reenku19@users.noreply.github.com>
2024-09-23 21:49:50 +0530

    Update Achievements.jsx

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import c1 from "../assets/c1.jpg";
-import c2 from "../assets/c2.png"
-import c3 from "../assets/c3.png"
+import a1 from "../assets/a1.png";
+import a2 from "../assets/a2.png"
+import a3 from "../assets/a3.png"
 
 
 export const Achievements = () => {
@@ -10,33 +10,33 @@ export const Achievements = () => {
             id: 1,
             name: "2nd PUC College Topper ",
             org: "Vidya Sankaar PU College",
-            image: c1,
+            image: a1,
         },
         {
             id: 2,
             name: "University 3rd Rank 2022 ",
             org: "Bengaluru City University",
-            image: c2,
+            image: a2,
         },
         {
             id: 3,
             name: "NSS Republic Day Parade",
             org: "Government of Karnataka",
-            image: c3,
+            image: a3,
         },
        
     ]
 
     return (
-        <div className="bg-black text-white py-20" id="achievement">
-            <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <div className="bg-black text-white py-30" id="achievement">
+            <div className="container mx-auto px-6 md:px-12 lg:px-18">
                 <h2 className="text-4xl font-bold text-center mb-12">Achievements</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {achievements.map((achievement) => (
-                        <div key={achievement.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
+                        <div key={achievement.id} className="bg-black p-4 rounded-lg hover:shadow-lg 
             transform transition-transform duration-300 hover:scale-105">
-                            <img src={achievement.image} alt={achievement.name} className="rounded-lg mb-4 
-              w-full h-48 object-cover" />
+                            <img src={achievement.image} alt={achievement.name} className="rounded-md mb-4 w-full
+               h-100 object-cover" />
                             <h3 className="text-2xl font-bold mb-2">{achievement.name}</h3>
                             <p className="text-blue-400 mb-4">{achievement.org}</p>
                         </div>
