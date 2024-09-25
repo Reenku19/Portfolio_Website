ef07f1c1285becab83f7705d5d9c234b29240f2b
Reenku Choudhary <105987075+Reenku19@users.noreply.github.com>
2024-09-25 14:12:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
  export const Navbar = () => {
   return (
     <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
-        <div className='container py-2 flex justify-center md:justify-between items-center'>
-            <div className='text-2xl font-bold hidden md:inline'>Reenkz</div>
-            <div className='space-x-6'>
+        <div className='container mx-auto py-2 flex flex-wrap justify-center md:justify-between items-center'>
+            <div className='text-white text-2xl font-bold sm:w-auto md:inline'>Reenkz</div>
+            <div className=' w-full lg:space-x-6 md:space-x-4 sm:space-x-2 sm:w-auto flex flex-wrap justify-between text-center'>
                 <a href="#hero" className='hover:text-gray-400'>Home</a>
                 <a href="#about" className='hover:text-gray-400'>About Me</a>
                 <a href="#project" className='hover:text-gray-400'>Projects</a>
